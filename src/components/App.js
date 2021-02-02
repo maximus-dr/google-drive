1133cec485c6cf4bf3aa65794c5d278e606a872f
Max Ivanov <maximus-dr@mail.ru>
2021-02-02 12:35:47 +0300

    Basic structure with auth template

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import SignUp from "./SignUp";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import AuthProvider from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
+
+
+function App() {
+  return (
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+}
+
+export default App;
